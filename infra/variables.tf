25bdc37c281c03895ef3f1ab56c5511c1b1cfac8
Guillermo Lam <guillermolam.m@gmail.com>
2026-04-04 21:52:21 +0200

    chore: add sensitive variables for tokens
    - Add github_token and tfe_token as sensitive variables
    - Update providers to use variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,21 @@ variable "default_branch" {
   type        = string
   default     = "main"
 }
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_owner" {
+  description = "GitHub organization or user"
+  type        = string
+  default     = "guillermolam"
+}
+
+variable "tfe_token" {
+  description = "Terraform Cloud token"
+  type        = string
+  sensitive   = true
+}
